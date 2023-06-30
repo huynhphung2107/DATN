68415983b41630f924cbe10249beb4ee568e7829
oanhnk <118040842+oanhnk@users.noreply.github.com>
2023-06-30 16:47:52 +0700

    Oanh hoan thien update khach hang

--- src/main/java/com/teamcode/datn/entity/KhachHang.java
+++ src/main/java/com/teamcode/datn/entity/KhachHang.java
@@ -2,7 +2,8 @@ package com.teamcode.datn.entity;
 
 public class KhachHang {
     public static void main(String[] args) {
-        System.out.println("hello ");
         System.out.println("helloworld");
+
+        
     }
 }
