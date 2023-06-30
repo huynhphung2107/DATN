6e1344bd8288cb89225dcde88b0ef3526e9cef23
huynhphung2107 <huynhphung2107@gmail.com>
2023-06-30 16:31:50 +0700

    them text

--- src/main/java/com/teamcode/datn/entity/NhanVien.java
+++ src/main/java/com/teamcode/datn/entity/NhanVien.java
@@ -3,4 +3,8 @@ package com.teamcode.datn.entity;
 public class NhanVien {
    private String id;
    private String maNhanVien;
+
+   public static void main(String[] args) {
+      System.out.println("Noong kieu oanh");
+   }
 }
