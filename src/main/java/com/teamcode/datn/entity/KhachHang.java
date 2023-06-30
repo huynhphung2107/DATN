ac65e0de3ce363246a0443190df360c516dca842
oanhnk <118040842+oanhnk@users.noreply.github.com>
2023-06-30 16:44:03 +0700

    Oanh update class Khachhang

--- src/main/java/com/teamcode/datn/entity/KhachHang.java
+++ src/main/java/com/teamcode/datn/entity/KhachHang.java
@@ -3,5 +3,6 @@ package com.teamcode.datn.entity;
 public class KhachHang {
     public static void main(String[] args) {
         System.out.println("hello ");
+        System.out.println("helloworld");
     }
 }
