eb4eaaae778351c76b4dc72d12c5c74109e2608a
hoangthithudiep <117933529+hoangthithudiep@users.noreply.github.com>
2023-07-04 22:25:17 +0700

    diệp dã hoàn thiện

--- src/main/java/com/teamcode/datn/entity/CuaHang.java
+++ src/main/java/com/teamcode/datn/entity/CuaHang.java
@@ -2,7 +2,6 @@ package com.teamcode.datn.entity;
 
 public class CuaHang {
     public static void main(String[] args) {
-        System.out.println("Them code moi nhat");
-        System.out.println("Them code moi nhat");
+        System.out.println("Đã sửa lai");
     }
 }
