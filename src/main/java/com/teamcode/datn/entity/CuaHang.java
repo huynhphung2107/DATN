2878f0ac3467023cdcc8a22ee75bccf578d6fd61
hoangthithudiep <117933529+hoangthithudiep@users.noreply.github.com>
2023-07-04 22:20:42 +0700

    Diệp thêm class Cua Hang

---
+++ src/main/java/com/teamcode/datn/entity/CuaHang.java
@@ -0,0 +1,8 @@
+package com.teamcode.datn.entity;
+
+public class CuaHang {
+    public static void main(String[] args) {
+        System.out.println("Them code moi nhat");
+        System.out.println("Them code moi nhat");
+    }
+}
