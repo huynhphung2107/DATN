1fb97ffabbae1092b0d3a2aba8b6fa4144e7cb8e
huynhphung2107 <huynhphung2107@gmail.com>
2023-07-09 17:39:09 +0700

    update giao diện

---
+++ src/main/java/com/teamcode/datn/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.teamcode.datn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/home")
+@Controller
+public class HomeController {
+
+    @GetMapping("")
+    public String home (Model model){
+        model.addAttribute("helo","WEBSITE BAN BALO THOI TRANG");
+        return "home";
+    }
+}
--- src/main/java/com/teamcode/datn/controller/NhanVienController.java
+++
@@ -1,4 +0,0 @@
-package com.teamcode.datn.controller;
-
-public class NhanVienController {
-}
