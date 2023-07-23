44808c366f33bd061fdf27f67779f7298dabab59
huynhphung2107 <huynhphung2107@gmail.com>
2023-07-23 22:50:46 +0700

    no message

--- src/main/java/com/teamcode/datn/controller/HomeController.java
+++ src/main/java/com/teamcode/datn/controller/HomeController.java
@@ -12,6 +12,6 @@ public class HomeController {
     @GetMapping("")
     public String home (Model model){
         model.addAttribute("helo","WEBSITE BAN BALO THOI TRANG");
-        return "home";
+        return "layout/giaodien";
     }
 }
