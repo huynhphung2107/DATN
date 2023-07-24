1ccbabc1fed7ca9270a3ad331fd475c49967fd85
huynhphung2107 <huynhphung2107@gmail.com>
2023-07-24 14:10:20 +0700

    no message

--- src/main/java/com/teamcode/datn/controller/SanPhamController.java
+++ src/main/java/com/teamcode/datn/controller/SanPhamController.java
@@ -2,10 +2,6 @@ package com.teamcode.datn.controller;
 
 public class SanPhamController {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hello");
-=======
-        System.out.println("okok");
->>>>>>> master
     }
 }
